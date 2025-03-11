ce508a818da251ad859c559ba04558ee1ff79203
Jami Geetha Lakshmi Sowmya <22MH1A0521@acoe.edu.in>
2025-03-11 10:56:53 +0530

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -58,3 +58,78 @@ cin>>num;
      }
 }
 //for(int i=2;i<n;i++)     time complexity is O(n).
+
+
+
+
+
+//GCD
+//CODE:1 (WITHOUT OPTIMIZATION)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    int min=0;
+    if(x>y) {
+        min=y;
+    } else {
+        min=x;
+        
+    }
+    for(int i=min;i>=1;i--) 
+    {
+        if(x%i==0 && y%i==0) {
+            cout<<"GCD is"<<" "<<i;
+        
+        break;
+    }
+    }
+}
+
+
+//CODE:2 //WITH OPTIMIZATION OF TIME COMPLEXITY
+//EUCLIDEAN'S THEOREM
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    while(x!=0 && y!=0) {
+        if(x>y) {
+            x=x%y;
+        } else {
+            y=y%x;
+        }  
+        
+    }
+    cout<<max(x,y);
+}
+
+
+//GCD AND LCM:
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    int a=x,b=y;
+    while(x!=0 && y!=0) {
+        if(x>y) {
+            x=x%y;
+        } else {
+            y=y%x;
+        }  
+        
+    }
+    cout<<"GCD:"<<" "<<max(x,y)<<endl;
+    if(x!=0) {
+    	cout<<"LCM:"<<" "<<(a*b)/x;
+	} else {
+		cout<<"LCM:"<<" "<<(a*b)/y;
+	}
+}
